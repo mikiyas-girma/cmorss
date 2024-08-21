df56cbd5c82cb85f4047b37c3cafaeaf57b1c1c1
SeM2x <houssem135.302@gmail.com>
2024-08-21 21:33:48 +0100

    export background images for game lounge, avatar scene, and game scene

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,4 +1,8 @@
 import Logo from '../../public/images/logo.png';
 import ThreeDLogo from '../../public/images/3d-tic-tac-logo.png';
+import gameLoungeBg from '../../public/backgrounds/game-lounge.jpeg';
+import avatarSceneBg from '../../public/backgrounds/avatar-scene.jpeg';
+import gameSceneBg from '../../public/backgrounds/game-scene.jpeg';
 
-export { Logo, ThreeDLogo };
+
+export { Logo, ThreeDLogo, gameLoungeBg, avatarSceneBg, gameSceneBg };
