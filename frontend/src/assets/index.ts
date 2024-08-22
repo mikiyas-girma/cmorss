49024d14fa72ef5a879a4498dca3c8fb59c4307d
SeM2x <houssem135.302@gmail.com>
2024-08-22 12:41:01 +0100

    Refactor asset paths to use absolute URLs

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,8 +1,8 @@
-import Logo from '../../public/images/logo.png';
-import ThreeDLogo from '../../public/images/3d-tic-tac-logo.png';
-import gameLoungeBg from '../../public/backgrounds/game-lounge.jpeg';
-import avatarSceneBg from '../../public/backgrounds/avatar-scene.jpeg';
-import gameSceneBg from '../../public/backgrounds/game-scene.jpeg';
+import Logo from '/images/logo.png';
+import ThreeDLogo from '/images/3d-tic-tac-logo.png';
+import gameLoungeBg from '/backgrounds/game-lounge.jpeg';
+import avatarSceneBg from '/backgrounds/avatar-scene.jpeg';
+import gameSceneBg from '/backgrounds/game-scene.jpeg';
 
 
 export { Logo, ThreeDLogo, gameLoungeBg, avatarSceneBg, gameSceneBg };
