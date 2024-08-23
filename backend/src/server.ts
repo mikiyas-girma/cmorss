8a0472214b900cb26132802991f5214a258a0e24
Rayane <rayanetoko@gmail.com>
2024-08-23 21:40:17 +0100

    define socket io event

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
+import http from 'http';
 import express from 'express';
+import { Server } from 'socket.io'
 import apiRouter from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
+const socket = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +15,7 @@ app.use('/api/v1', apiRouter);
 
 async function startServer() {
   try {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch(error) {
