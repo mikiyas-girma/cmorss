ffbdbabcf933daf54d605411b988fc10e0fe97ae
SeM2x <houssem135.302@gmail.com>
2024-08-26 15:31:12 +0100

    Refactor socket event handling and error handling in io.ts

--- backend/src/io.ts
+++ backend/src/io.ts
@@ -1,27 +1,27 @@
 import http from 'http';
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import { SocketEvent } from './utils/socketEvents.js';
 
 let cachedIo: Server | null = null;
 
-// async function joinSocketToRoom (socket: Socket, roomId: string) {
-// 	try {
-// 		await socket.join(roomId);
-// 		console.log(`User ${socket.id} joined room ${roomId}`);
-// 		cachedIo.emit(SocketEvent.ROOM_JOINED);
-// 	} catch (error) {
-// 		console.error(`Invalid room id: ${roomId}`);
-// 		socket.emit(SocketEvent.ERROR, { error });
-// 	}
-// }
+async function joinSocketToRoom (socket: Socket, roomId: string) {
+	try {
+		await socket.join(roomId);
+		console.log(`User ${socket.id} joined room ${roomId}`);
+		cachedIo?.emit(SocketEvent.ROOM_JOINED, roomId, socket.id);
+	} catch (error) {
+		console.error(`Invalid room id: ${roomId}`);
+		socket.emit(SocketEvent.ERROR, { error });
+	}
+}
 
 // function getRoom(roomId: string) {
 // 	return !!cachedIo.sockets.adapter.rooms[roomId];
 // }
 
-// function checkMovePosition(position: [number, number]) {
-// 	return position[0] > 0 && position[0] <= 3 && position[1] > 0 && position[1] <= 3
-// }
+function checkMovePosition(position: [number, number]) {
+	return position[0] > 0 && position[0] < 3 && position[1] > 0 && position[1] < 3
+}
 
 function initSocketIo(httpServerInstance: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>) {
 	if (cachedIo) {
@@ -37,44 +37,43 @@ function initSocketIo(httpServerInstance: http.Server<typeof http.IncomingMessag
 			console.log('A user connected');
 			//TODO: Manage user session
 
-			// socket.on(SocketEvent.CREATE_ROOM, async (roomId: string) => {
-			// 	const roomExists = getRoom(roomId);
+			socket.on(SocketEvent.CREATE_ROOM, async (roomId: string) => {
+				//const roomExists = getRoom(roomId);
 
-			// 	if (roomExists) {
-			// 		console.error(`Room ${roomId} already exist.`);
-			// 		socket.emit(SocketEvent.ERROR, { message: 'Room already exist.' });
-			// 	} else {
-			// 		joinSocketToRoom(socket, roomId);
-			// 	}
-			// });
+				if (false) {
+					console.error(`Room ${roomId} already exist.`);
+					socket.emit(SocketEvent.ERROR, { message: 'Room already exist.' });
+				} else {
+					joinSocketToRoom(socket, roomId);
+				}
+			});
 
-			// socket.on(SocketEvent.JOIN_ROOM, async (roomId: string) => {
-			// 	const roomExists = getRoom(roomId);
+			socket.on(SocketEvent.JOIN_ROOM, async (roomId: string) => {
+				//const roomExists = getRoom(roomId);
 
-			// 	if (!roomExists) {
-			// 		console.error(`Room ${roomId} does not exist.`);
-			// 		socket.emit(SocketEvent.ERROR, { message: 'Room does not exist.' });
-			// 	} else {
-			// 		joinSocketToRoom(socket, roomId);
-			// 	}
-			// });
+				if (false) {
+					console.error(`Room ${roomId} does not exist.`);
+					socket.emit(SocketEvent.ERROR, { message: 'Room does not exist.' });
+				} else {
+					joinSocketToRoom(socket, roomId);
+				}
+			});
 
-			// socket.on(SocketEvent.MAKE_MOVE, (moveData: {
-			// 	position: [number, number],
-			// 	roomId: string
-			// }) => {
-			// 	if (checkMovePosition(moveData.position) && socket.rooms.has(moveData.roomId)) {
-			// 		const emited = cachedIo.to(moveData.roomId).emit(SocketEvent.MOVE_MADE, moveData);
+			socket.on(SocketEvent.MAKE_MOVE, (moveData: {
+				position: [number, number],
+				roomId: string
+			}) => {
+				if (checkMovePosition(moveData.position) && socket.rooms.has(moveData.roomId)) {
+					const emited = cachedIo?.to(moveData.roomId).emit(SocketEvent.MOVE_MADE, moveData);
 
-			// 		if (!emited) {
-			// 				console.error(`Failled to emit event for user ${socket.id}`);
-			// 				socket.emit(SocketEvent.ERROR, { message: 'Move made not shared.' });
-			// 		}
-			// 	}
-			// });
+					if (!emited) {
+							console.error(`Failled to emit event for user ${socket.id}`);
+							socket.emit(SocketEvent.ERROR, { message: 'Move made not shared.' });
+					}
+				}
+			});
 
 			socket.on(SocketEvent.DISCONNECT, () => {
-				socket.disconnect();
 				console.log('User disconnected');
 			});
 
